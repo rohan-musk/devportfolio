6b8b6fd6379e8471c941fe9b738f5df07999a321
Rohan Muskawad <84379921+rohan-musk@users.noreply.github.com>
2022-02-20 12:17:43 +0530

    adding basic navbar function

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './css/Navbar.css'
+const Navbar = () => {
+    return (
+        <div class="Navbar">
+            <h3>Rohan Musk</h3>
+            <ul>
+                <li><a href="https://github.com/rohan-musk" target="_blank">GitHub</a></li>
+                <li><a href="https://www.instagram.com/rohanmusk/" target="_blank">Instagram</a></li>
+                <li><a href="https://twitter.com/MuskawadRohan" target="_blank">Twitter</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
